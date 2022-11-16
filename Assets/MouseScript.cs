b98214cfd9841649277c4e58e0787bfe7e386125
Peter Coleman <plc0016@cs.uah.edu>
2022-11-16 12:31:41 -0600

    Implement basic player object interactions

---
+++ Assets/MouseScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseScript : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float move_timer;
+    private bool move_right;
+    float move_length = 2.0f;
+    float mouse_speed = 2.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        move_timer = move_length/2;
+        move_right = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vel = rb.velocity;
+        move_timer -= Time.deltaTime;
+        if(move_timer < 0)
+        {
+            move_right = !move_right;
+            move_timer = move_length;
+        }
+        if(move_right) {
+            vel.x = mouse_speed;
+        } else {
+            vel.x = -mouse_speed;
+        }
+        rb.velocity = vel;
+    }
+}
--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -11,7 +11,6 @@ public class PlayerScript : MonoBehaviour
     
 
     private Rigidbody rb;
-    private Vector3 vel;
     private bool stopped;
 
     // Start is called before the first frame update
@@ -60,7 +59,7 @@ public class PlayerScript : MonoBehaviour
                 }
             }
         }
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+        if(Physics.Raycast(transform.position, Vector3.down, 1.01f) && Input.GetKeyDown(KeyCode.UpArrow))
         {
             vel.y = jump_speed;
         }
@@ -72,7 +71,65 @@ public class PlayerScript : MonoBehaviour
     {
         Vector3 pos = Camera.main.transform.position;
         pos.x = transform.position.x;
-        pos.y = transform.position.y;
         Camera.main.transform.position = pos;
+    }
+
+    private void StartFall()
+    {
+        Vector3 vel = rb.velocity;
+        vel.y = 0;
+        rb.velocity = vel;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "enemy")
+        {
+            //Do not detect collisions that are highly horizontal
+            if(collision.contacts[0].normal.y > 0.25f) {
+                //Destroy enemy
+                Destroy(collision.gameObject);
+                //Start falling
+                StartFall();
+            } else {
+                //Destroy player
+                Destroy(gameObject);
+            }
+        }
+        if(collision.gameObject.tag == "spike")
+        {
+            if(collision.contacts[0].normal.y > 0.01f)
+            {
+                //Destroy player
+                Destroy(gameObject);
+            }
+        }
+        if(collision.gameObject.tag == "block")
+        {
+            //Detect highly downward collisions
+            if(collision.contacts[0].normal.y < -0.8f)
+            {
+                //Destroy brick
+                Destroy(collision.gameObject);
+                //Start falling
+                StartFall();
+            }
+        }
+        if(collision.gameObject.tag == "item_block")
+        {
+            //Detect highly downward collisions
+            if (collision.contacts[0].normal.y < -0.8f)
+            {
+                //Make block orange
+                Renderer renderer_temp = collision.gameObject.GetComponent<Renderer>();
+                Color orange = new Color(1f, 0.65f, 0);
+                if(renderer_temp.material.color != orange)
+                {
+                    renderer_temp.material.color = orange;
+                }
+                //Start falling
+                StartFall();
+            }
+        }
     }
 }
