f1db87edc9b460085a273d8d4fc254543c7313b8
gamemasterplc <gamemasterplc@gmail.com>
2022-11-27 08:47:45 -0600

    Add cheese cannon code

---
+++ Assets/CheeseCannonScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheeseCannonScript : MonoBehaviour
+{
+    [Header("Inspector-Set Value:")]
+    public GameObject cheese_object;
+    public float max_shoot_timer = 1.5f;
+    private float shoot_timer;
+
+    void Start()
+    {
+        shoot_timer = max_shoot_timer;
+    }
+
+    void Update()
+    {
+        shoot_timer -= Time.deltaTime;
+        if(shoot_timer < 0) {
+            Vector3 cheese_pos = transform.position;
+            cheese_pos.x -= 1.0f;
+            cheese_pos.y += 0.625f;
+            GameObject temp = Instantiate<GameObject>(cheese_object);
+            temp.transform.position = cheese_pos;
+            shoot_timer = max_shoot_timer;
+        }
+    }
+}
---
+++ Assets/CheeseScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheeseScript : MonoBehaviour
+{
+    void Start()
+    {
+        //Move cheese at 9 units/second
+        GetComponent<Rigidbody>().velocity = new Vector3(-9.0f, 0.0f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "player") {
+            //Take damage if player hits cheese
+            PlayerScript player = collision.gameObject.GetComponent<PlayerScript>();
+            player.TakeDamage();
+        }
+        Destroy(gameObject);
+    }
+}
