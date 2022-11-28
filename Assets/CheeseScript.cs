999dd0de76bb5048346b3413b3b8e1018a7d242e
gamemasterplc <gamemasterplc@gmail.com>
2022-11-27 18:41:29 -0600

    Make cheese last for limited time

--- Assets/CheeseScript.cs
+++ Assets/CheeseScript.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class CheeseScript : MonoBehaviour
 {
+    private float timer;
+
     void Start()
     {
         //Move cheese at 9 units/second
         GetComponent<Rigidbody>().velocity = new Vector3(-9.0f, 0.0f);
+        timer = 7.0f; //Cheese exists for 7 seconds
     }
 
+    private void Update()
+    {
+        //Destroy cheese when timer expires
+        timer -= Time.deltaTime;
+        if(timer < 0) {
+            Destroy(gameObject);
+        }
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "player") {
